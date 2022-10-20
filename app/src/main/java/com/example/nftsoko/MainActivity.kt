f9868b7d01129501e7b8d67de9f5764c5bdd46fd
elkana-m <munganga.elkana@yahoo.fr>
2022-10-20 01:07:28 -0500

    completed Onboarding screen & added res files

--- app/src/main/java/com/example/nftsoko/MainActivity.kt
+++ app/src/main/java/com/example/nftsoko/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.nftsoko
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.nftsoko.ui.theme.NFTSokoTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Text("Hello Jetpack Compose!")
                 }
             }
         }
@@ -30,14 +40,94 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun OnboardingCard() {
+    Scaffold{
+        Box{
+            Image(
+                painter = painterResource(id = R.drawable.bg1),
+                contentDescription = "Background Image",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                Modifier
+                    .padding(horizontal = 32.dp, vertical = 80.dp)
+                    .fillMaxSize()
+            ){
+                Text(
+                    "Welcome to NFT Marketplace",
+                    color = Color.White,
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Black
+                )
+                Spacer(Modifier.fillMaxHeight(0.65f))
+                Card(
+                    elevation = 4.dp,
+                    modifier = Modifier
+                        .border(
+                            width = 1.dp,
+                            color = Color.White.copy(0.1f),
+                            shape = RoundedCornerShape(27.dp)
+                        )
+                        .clip(RoundedCornerShape(27.dp))
+                ){
+                    Image(
+                        painter = painterResource(id = R.drawable.card_blur),
+                        contentDescription = "Card Background",
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
+                    )
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(27 .dp)
+                    ) {
+                        Text(
+                            "Explore and Mint NFTs",
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp,
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            "You can buy and sell the NFTs of the best artists in the world.",
+                            color = Color.White.copy(0.8f),
+                            textAlign = TextAlign.Center
+                        )
+                        Spacer(modifier = Modifier.padding(bottom = 27.dp))
+                        Button(
+                            onClick = {/*TODO*/ },
+                            shape = RoundedCornerShape(percent = 50),
+                            modifier = Modifier.border(
+                                width = 1.dp,
+                                color = Color.White.copy(0.5f),
+                                shape = RoundedCornerShape(percent = 50)
+                            ),
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = Color(151, 169, 246, alpha = 0x32),
+                                contentColor = Color.White
+                            )
+                        ){
+                            Text(
+                                "Get started now",
+                                modifier = Modifier.padding(horizontal = 40.dp, vertical = 4.dp),
+                                fontSize = 15.sp,
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        }
+                    }
+                }
+
+
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
+fun CardPreview() {
     NFTSokoTheme {
-        Greeting("Android")
+        OnboardingCard()
+
     }
 }
